81db77a0ffc338bde24b0bfe6f3232aba1d49472
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 21:48:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,13 @@
 # Bitunix API
 API_KEY = "1ed4f8296ada5b2ca96b19f479e57a75"
 API_SECRET = "8878c5af67568fc92f9986f518c1b646"
-
 BASE_URL = "https://fapi.bitunix.com"
-SYMBOL = "BTCUSDT"
-STAKE_EURO = 10         
+
+SYMBOL = "BRETTUSDT"
+MARGIN_COIN = "USDT"
+STAKE_USD = 10  # Bedrag in USD per trade
 LEVERAGE = 10
+ORDER_TYPE = 1  # 1 = market
+POSITION_SIDE_LONG = 1
+POSITION_SIDE_SHORT = 2
 
