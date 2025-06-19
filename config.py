9a472f7da6901f5698719a7330e03d961b2fa153
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 16:03:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@
 # Bitunix API
 API_KEY = "c77c52561af677e83d0fa9284f478039"
 API_SECRET = "03856b04f28609cf9bdaa5dd75370466"
+BASE_URL = "https://openapi.bitunix.com"
 
 # Telegram
 BOT_TOKEN = "7976964802:AAGXpRIP4usH51ceVIBcYltOWAN7PC9E2AI"
 TELEGRAM_CHAT_ID = "1927350412"
 
-# Bot instellingen
+# Trading instellingen
 SYMBOL = "BRETTUSDT"
-STAKE_EURO = 10
+VOLUME = 81300813.01  # aanpasbaar volume
 LEVERAGE = 10
+ORDER_TYPE = 1  # Market order
 STOP_LOSS_PERCENT = 0.5  # zonder leverage
 TAKE_PROFIT_PERCENT = 0.3
 TRAILING_BUFFER_PERCENT = 0.1
 
-# Bitunix endpoint
-BASE_URL = "https://openapi.bitunix.com"
