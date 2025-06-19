e6e4b0f8c1a86f7740bcc72546e299848de23cfc
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 18:54:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # Bitunix API
-API_KEY = "c77c52561af677e83d0fa9284f478039"
-API_SECRET = "03856b04f28609cf9bdaa5dd75370466"
+API_KEY = "1ed4f8296ada5b2ca96b19f479e57a75"
+API_SECRET = "8878c5af67568fc92f9986f518c1b646"
 BASE_URL = "https://fapi.bitunix.com"
 
 # Trading parameters
