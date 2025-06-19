9273dd66c31e18d92d1a4010b1792b12e3849444
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 21:00:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_KEY = "1ed4f8296ada5b2ca96b19f479e57a75"
 API_SECRET = "8878c5af67568fc92f9986f518c1b646"
 
 BASE_URL = "https://fapi.bitunix.com"
-SYMBOL = "BRETTUSDT"
+SYMBOL = "BTCUSDT"
 STAKE_EURO = 10         
 LEVERAGE = 10
 
