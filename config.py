2b0728be39eff4200fed3c9919b684b2762b0cbf
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 22:38:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,6 @@
 API_KEY = "1ed4f8296ada5b2ca96b19f479e57a75"
 API_SECRET = "8878c5af67568fc92f9986f518c1b646"
 BASE_URL = "https://fapi.bitunix.com"
-
 SYMBOL = "BRETTUSDT"
-
-STAKE_EURO = 10  # bijvoorbeeld $10
 LEVERAGE = 10
-
-ORDER_TYPE = 1  # 1 = Market
-POSITION_SIDE_LONG = 1
-POSITION_SIDE_SHORT = 2
-
-BASE_PRECISION = 0  # Voor BRETT is dit 0 volgens info uit trading_pairs
-
-
+STAKE_EURO = 10  # In euro/dollar bedrag
