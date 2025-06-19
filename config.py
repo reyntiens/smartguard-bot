1940e207bb9f485eefbbab8dc4f2cc001301cae3
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 18:45:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,13 @@ API_KEY = "c77c52561af677e83d0fa9284f478039"
 API_SECRET = "03856b04f28609cf9bdaa5dd75370466"
 BASE_URL = "https://fapi.bitunix.com"
 
-# Trading instellingen
+# Trading parameters
 SYMBOL = "BRETTUSDT"
-VOLUME = 81300813.01  # of kleiner bedrag voor test
 LEVERAGE = 10
-ORDER_TYPE = 1  # market order
+ORDER_TYPE = 1  # 1 = Market Order
 POSITION_SIDE_LONG = 1
 POSITION_SIDE_SHORT = 2
+VOLUME = 81300813.01  # Pas dit aan naar wens
 
-# Telegram
-BOT_TOKEN = "7976964802:AAGXpRIP4usH51ceVIBcYltOWAN7PC9E2AI"
-TELEGRAM_CHAT_ID = "1927350412"
-
-# Risicobeheer
-STOP_LOSS_PERCENT = 0.5
-TAKE_PROFIT_PERCENT = 0.3
-TRAILING_BUFFER_PERCENT = 0.1
 
 
