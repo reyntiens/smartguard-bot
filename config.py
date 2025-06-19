f5ea4aae31b21b0ec934f70b6ed606f2f60e4d4f
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 22:14:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,14 @@ API_SECRET = "8878c5af67568fc92f9986f518c1b646"
 BASE_URL = "https://fapi.bitunix.com"
 
 SYMBOL = "BRETTUSDT"
-MARGIN_COIN = "USDT"
-STAKE_USD = 10  # Bedrag in USD per trade
+
+STAKE_EURO = 10  # bijvoorbeeld $10
 LEVERAGE = 10
-ORDER_TYPE = 1  # 1 = market
+
+ORDER_TYPE = 1  # 1 = Market
 POSITION_SIDE_LONG = 1
 POSITION_SIDE_SHORT = 2
+
+BASE_PRECISION = 0  # Voor BRETT is dit 0 volgens info uit trading_pairs
+
 
