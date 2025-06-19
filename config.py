230ea9a3877ccd8d8cef9cefb29bbf500260c1f0
reyntiens <77543764+reyntiens@users.noreply.github.com>
2025-06-19 18:57:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,9 @@ API_KEY = "1ed4f8296ada5b2ca96b19f479e57a75"
 API_SECRET = "8878c5af67568fc92f9986f518c1b646"
 BASE_URL = "https://fapi.bitunix.com"
 
-# Trading parameters
 SYMBOL = "BRETTUSDT"
+VOLUME = 10
 LEVERAGE = 10
-ORDER_TYPE = 1  # 1 = Market Order
+ORDER_TYPE = 1  # Marktorder
 POSITION_SIDE_LONG = 1
 POSITION_SIDE_SHORT = 2
-VOLUME = 81300813.01  # Pas dit aan naar wens
-
-
-
